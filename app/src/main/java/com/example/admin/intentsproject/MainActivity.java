db6acc3aabf61a72ccac3b60cf307f96fff9ecd1
eguegan <e.guegan@margulls.com>
2016-03-31 16:36:53 -0400

    Add intent to second activity

--- app/src/main/java/com/example/admin/intentsproject/MainActivity.java
+++ app/src/main/java/com/example/admin/intentsproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.intentsproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doMagic(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
